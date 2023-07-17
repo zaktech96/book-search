d54dcc1afafc80864c07fd00cfdfb6939be834b8
zaktech96 <zakariyesahid96@gmail.com>
2023-07-18 00:59:15 +0100

    Added Use effect hook with fetchBooks request

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
   };
   return (
     <div className="app">
-      <h1>Reading titles</h1>
+      <h1>Reading Book</h1>
       <BookList
         onEdit={editBookById}
         books={books}
